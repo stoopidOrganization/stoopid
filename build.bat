340bc0a74c13e12c022c9243d4646e6ecf14cb24
nils865 <nils.staas0806@gmail.com>
2022-08-21 19:17:14 +0200

    New Build

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
-pyinstaller --noconfirm --onefile --console --icon "images/stoopidlogo.ico" --add-data "config.json;."  "stoopid.py"
+pyinstaller --noconfirm --onefile --console --icon "images/stoopidlogo.ico"  "stoopid.py"
 move dist\stoopid.exe stoopid.exe
-echo Starting cleanup...
+echo Starting cleanup...    
 del dist /F /Q /S
 del build /F /Q /S
 rmdir build /S /Q
