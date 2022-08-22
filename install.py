083a1ed2ab5e15bbda6905bca92bb2e0539497aa
kruemmelbande <kai.scheufen@gmail.com>
2022-08-22 12:13:25 +0200

    Fixed the path delete issue (yes i lied about not deleting the path)

--- install.py
+++ install.py
@@ -16,13 +16,15 @@ for file in files:
     os.system("copy %s %s" % (file,installdir))
 #add the path to the environment variables
 user=os.getenv("USERNAME")
-cpath=os.getenv("PATH")
-
+#get the path for the current user
+cpath=os.popen("reg query \"HKCU\Environment\" /v PATH").read()
+cpath=cpath.split("REG_SZ")[1].strip()
+print(cpath)
 if not installdir in cpath and input("Do you want to add stoopid to path? [y/n]").strip()=="y":
     cpath+=";%s" % installdir
-    #print(cpath)
+    print(cpath)
     command="setx PATH \"%s\"" % cpath
-    #print(command)
+    print(command)
     os.system(command)
     print("Added %s to PATH" % installdir)
     print("You can now run stoopid by typing stoopid in your terminal")
